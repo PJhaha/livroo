6b4c9be9d48f718d8a428da389ccb904b4834087
PJhaha <163062179+PJhaha@users.noreply.github.com>
2024-03-21 22:13:48 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    
+
+    if(imagem.classList.contains('dashboard__item__img--rented')){
+        
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
+        alert('Você devolveu com sucesso Martha!')
+        
+    }else{
+
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+        alert('Isso aí, agora tenta devolver')
+        
+    }
+}
